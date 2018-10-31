ff2e7673c44a6f1f3cb97734d7f11a7dffd1f918
Wouter <wouter.feremans@student.kuleuven.be>
2018-10-31 14:39:49 +0100

    standard login page

--- application/models/menu_model.php
+++ application/models/menu_model.php
@@ -15,10 +15,10 @@ class Menu_model extends CI_Model {
         parent::__construct();
         // <a href="link" title="title" class="className">name</a>
         $this->menu_items = array (
-            array('name'=>'Home', 'title'=>'Go home', 'link'=>'home', 'className'=>'active'),
-            array('name'=>'Tips', 'title'=>'Look for the tips', 'link'=>'tips', 'className'=>'inactive'),
+            array('name'=>'Home', 'title'=>'Go home', 'link'=>'index', 'className'=>'active'),
+            array('name'=>'Tips', 'title'=>'Look for the tips', 'link'=>'#', 'className'=>'inactive'),
             array('name'=>'Upcoming events', 'title'=>'Have a look at the upcoming events', 'link'=>'events', 'className'=>'inactive'),
-            array('name'=>'Create', 'title'=>'Start a new potluck event', 'link'=>'create', 'className'=>'inactive'),
+            array('name'=>'Create', 'title'=>'Start a new potluck event', 'link'=>'#', 'className'=>'inactive'),
             array('name'=>'About', 'title'=>'About this website', 'link'=>'about', 'className'=>'inactive'),
         );
     }
---
+++ application/views/ForgotPW.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>{page_title}</title>
+    <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>assets/images/hospital-3.png">
+</head>
+<body>
+
+</body>
+</html>
