e2ab58f8ab723d1deee14fcfddaf16ebe4281671
Wouter <wouter.feremans@student.kuleuven.be>
2018-11-03 20:53:32 +0100

    changed mysqli driver to pdo_mysql driver so it works on the studev server

--- application/config/database.php
+++ application/config/database.php
@@ -72,17 +72,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+/*
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'mysql.studev.groept.be',
 	'username' => 'a18uxwd',
 	'password' => 'a18uxwd',
 	'database' => 'a18uxwd',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);*/
+
+$db['default'] = array(
+	'dsn'	=> 'mysql:host=mysql.studev.groept.be;dbname=a18uxwd',
+	'hostname' => '',
+	'username' => 'a18uxwd',
+	'password' => 'a18uxwd',
+	'database' => '',
+	'dbdriver' => 'pdo',
+	'dbprefix' => '',
+	'pconnect' => TRUE,
+	'db_debug' => TRUE,
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
