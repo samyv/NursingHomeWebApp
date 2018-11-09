5ce5225e65c386daf28dd8dc8518ab838938abc9
Wouter <wouter.feremans@student.kuleuven.be>
2018-11-09 16:29:20 +0100

    layout login screen

--- application/views/Caregiver/login.php
+++ application/views/Caregiver/login.php
@@ -3,47 +3,53 @@
 <head>
     <title>{page_title}</title>
     <link href="<?=base_url()?>assets/css/login.css" rel="stylesheet" type="text/css">
-    <link href="<?=base_url()?>assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <!--<link href="<?=base_url()?>assets/css/bootstrap.css" rel="stylesheet" type="text/css"> -->
     <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>assets/images/logo.png">
 </head>
 <body>
-<div class="logo">
-    <h1>GraceAge</h1>
-    <h2>Providing better care</h2>
-</div>
+    <div class="logo">
+        <h1>GraceAge</h1>
+        <h2>Providing better care</h2>
+    </div>
 
-<div class="container">
-    <?php
-    if(!empty($success_msg)){
-        echo '<p class="statusMsg">'.$success_msg.'</p>';
-    }elseif(!empty($error_msg)){
-        echo '<p class="statusMsg">'.$error_msg.'</p>';
-    }
-    ?>
-    <form action="" method="POST">
-            <label for="email"><b>Email</b></label>
-            <br>
-            <div class="form-group has-feedback">
-                <input id="emailField" class="form-control" type="email" placeholder="Enter email" name="email" required="" value="">
-                <?php echo form_error('email','<span class="help-block">','</span>'); ?>
-            </div>
-            <br>
-            <label for="psw"><b>Password</b></label>
-            <br>
-            <div class="form-group">
-                <input type="password" class="form-control" placeholder="Enter Password" name="password" required="">
-                <?php echo form_error('password','<span class="help-block">','</span>'); ?>
-            </div>
-            <br>
-            <span class="psw"><a href="#" id="pwdForgot">Forgot password?</a></span>
-            <div class="form-group">
-                <input type="submit" name="loginSubmit" class="btn-primary" value="Submit"/>
-            </div>
-    </form>
-    <input type="submit" name="regisSubmit" class="btn-primary" value="Register" onclick="location.href='register'"/>
-</div>
+    <div class="container">
+        <div class="image">
+            <img id="remyImg" src="<?=base_url();?>/assets/images/edouard-remy.png" >
+        </div>
 
+        <div class="login-form">
+            <h1>Login</h1>
+            <?php
+            if(!empty($success_msg)){
+                echo '<p class="statusMsg">'.$success_msg.'</p>';
+            }elseif(!empty($error_msg)){
+                echo '<p class="statusMsg">'.$error_msg.'</p>';
+            }
+            ?>
+            <form action="" method="POST">
+                    <label for="email"><b>Email</b></label>
+                    <br>
+                    <div class="form-group has-feedback">
+                        <input id="emailField" class="form-control" type="email" placeholder="Enter email" name="email" required="" value="">
+                        <?php echo form_error('email','<span class="help-block">','</span>'); ?>
+                    </div>
+                    <br>
+                    <label for="psw"><b>Password</b></label>
+                    <br>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Enter Password" name="password" required="">
+                        <?php echo form_error('password','<span class="help-block">','</span>'); ?>
+                    </div>
+                    <br>
+                    <span class="psw"><a href="#" id="pwdForgot">Forgot password?</a></span>
+                    <div class="form-group">
+                        <input type="submit" name="loginSubmit" class="btn-primary" value="Login"/>
+                        <input type="submit" class="btn-primary" value="Register" onclick="location.href='register'"/>
+                    </div>
+            </form>
+        </div>
 
+    </div>
 <!--modal-->
 <div id="pwdModal" class="modal">
     <div class="modal-dialog">
