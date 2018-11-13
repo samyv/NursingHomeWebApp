0e75271da60dfccef18bc0201fc5678d944fd2fa
harpa.thrastardottir <harpa.thrastardottir@student.kuleuven.be>
2018-11-13 16:34:54 +0100

    init landing page

---
+++ application/views/Caregiver/landingPage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="<?php echo base_url(); ?>assets/css/landingPage.css" rel='stylesheet' type='text/css' />
+    <link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>assets/images/logo.png">
+    <title>{page_title}</title>
+</head>
+<body>
+<div class="grid-container">
+
+    <div class = "title">
+        <h3>Welcome back <?php echo $caregiver['firstname'], " ", $caregiver['lastname'] ; ?>!</h3>
+    </div>
+
+
+
+</div>
+</body>
+</html>
