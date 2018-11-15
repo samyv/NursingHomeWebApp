b61610a02d59c9a30efcbd48397262beaccaedf5
samy.vanvooren <samy.vanvooren@student.kuleuven.be>
2018-11-15 21:35:07 +0100

    search resident is done!

--- application/views/searchForResident.php
+++ application/views/searchForResident.php
@@ -3,7 +3,8 @@
 <head>
     <title>Database Searching</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script src="javascript/search.js"></script>
+<!--    <script src="javascript/search.js"></script>-->
+
     <link rel="stylesheet" href="assets/css/searchForResident.css">
 </head>
 <body>
@@ -15,11 +16,45 @@
 <?php
 ?>
 <ul id="myUL">
-	{listCar}
-	<li>{firstname} {lastname}</li>
-	{/listCar}
+
 </ul>
 
+<script>
+	window.onload = populate;
 
+	function search() {
+		var input, filter, ul, li, a, i;
+		input = document.getElementById("myInput");
+		filter = input.value.toUpperCase();
+		ul = document.getElementById("myUL");
+		li = ul.getElementsByTagName("li");
+		for (i = 0; i < li.length; i++) {
+			a = li[i].getElementsByTagName("a")[0];
+			if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+			}
+		}
+	}
+
+var database;
+	var list;
+
+	function populate()
+	{
+
+		database = <?php echo json_encode($listCar)?>;
+		list = document.getElementById("myUL");
+
+		for (var i = 0 ; i < database.length ; i++)
+		{
+			var element = document.createElement('li');
+			var name = database[i]['firstname'] +" " + database[i]['lastname'];
+			element.innerHTML = '<a href="#">' + name + '</a>';
+			list.appendChild(element);
+		}
+	}
+</script>
 </body>
 </html>
