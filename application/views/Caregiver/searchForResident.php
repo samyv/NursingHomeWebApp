d26f92c19cc700d6a7870f5b41b090a2b1a41f19
samy.vanvooren <samy.vanvooren@student.kuleuven.be>
2018-11-20 15:13:05 +0100

    refactor searchpage

--- application/views/Caregiver/searchForResident.php
+++ application/views/Caregiver/searchForResident.php
@@ -1,101 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Database Searching</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<!--    <script src="javascript/search.js"></script>-->
+	<title>Database Searching</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/searchForResident.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!--    <script src="javascript/search.js"></script>-->
 
-    <link rel="stylesheet" href="<?=base_url();?>assets/css/searchForResident.css">
 </head>
+
 <body>
 
 <h1>Database searching</h1>
 
 <body>
 <input type="text" id="myInput" onkeyup="search()" placeholder="Search for names.." title="Type in a name">
+<div class="carlist">
+	<table id="myTable"></table>
+</div>
 
-<!--<ul id="myUL">-->
-<!---->
-<!--</ul>-->
-
-<table id="myTable">
-	<tr>
-		<th>Name</th>
-		<th>Floor</th>
-		<th>Age</th>
-	</tr>
-	<tr>
-		<th>Company</th>
-		<th>Contact</th>
-		<th>Country</th>
-	</tr>
-	<tr>
-		<td>Alfreds Futterkiste</td>
-		<td>Maria Anders</td>
-		<td>Germany</td>
-	</tr>
-	<tr>
-		<td>Berglunds snabbköp</td>
-		<td>Christina Berglund</td>
-		<td>Sweden</td>
-	</tr>
-	<tr>
-		<td>Centro comercial Moctezuma</td>
-		<td>Francisco Chang</td>
-		<td>Mexico</td>
-	</tr>
-	<tr>
-		<td>Ernst Handel</td>
-		<td>Roland Mendel</td>
-		<td>Austria</td>
-	</tr>
-	<tr>
-		<td>Island Trading</td>
-		<td>Helen Bennett</td>
-		<td>UK</td>
-	</tr>
-	<tr>
-		<td>Königlich Essen</td>
-		<td>Philip Cramer</td>
-		<td>Germany</td>
-	</tr>
-	<tr>
-		<td>Laughing Bacchus Winecellars</td>
-		<td>Yoshi Tannamuri</td>
-		<td>Canada</td>
-	</tr>
-	<tr>
-		<td>Magazzini Alimentari Riuniti</td>
-		<td>Giovanni Rovelli</td>
-		<td>Italy</td>
-	</tr>
-	<tr>
-		<td>North/South</td>
-		<td>Simon Crowther</td>
-		<td>UK</td>
-	</tr>
-	<tr>
-		<td>Paris spécialités</td>
-		<td>Marie Bertrand</td>
-		<td>France</td>
-	</tr>
-</table>
 
 <script>
-	window.onload = populate;
-
+	window.onload = function (ev) {
+		populate();
+		init();
+	};
+	var list,name;
 	function search() {
-		var input, filter, ul, li, a, i;
+		var input, filter, table, i;
 		input = document.getElementById("myInput");
 		filter = input.value.toUpperCase();
-		ul = document.getElementById("myUL");
-		li = ul.getElementsByTagName("li");
-		for (i = 0; i < li.length; i++) {
-			a = li[i].getElementsByTagName("a")[0];
-			if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-				li[i].style.display = "";
-			} else {
-				li[i].style.display = "none";
+		table = document.getElementById("myTable");
+		list = document.getElementsByTagName('tr');
+		for (i = 1; i <= list.length; i++) {
+			// console.log(list[i])
+			if(list[i] != undefined) {
+				name = list[i].getElementsByTagName("td")[1].innerHTML;
+				// console.log(name);
+
+				if (name.toUpperCase().indexOf(filter) > -1) {
+					list[i].style.display = "";
+				} else {
+					list[i].style.display = "none";
+				}
 			}
 		}
 	}
@@ -105,32 +52,78 @@
 		var database = "";
 		database = <?php echo json_encode($listCar)?>;
 		var table = document.getElementById("myTable");
+		var tbody = document.createElement("tbody");
+		var row = document.createElement('tr');
+		var id = document.createElement('th');
+		// id.style.display = "block";
+		id.innerHTML = "ID"
+		row.appendChild(id);
 
+		var col1 = document.createElement('th');
+		col1.innerHTML = "Name";
+		row.appendChild(col1)
+		var col2 = document.createElement('th');
+		col2.innerHTML = "Floor";
+		row.appendChild(col2)
+		var col3 = document.createElement('th');
+		col3.innerHTML = "Age";
+		row.appendChild(col3)
+		tbody.appendChild(row)
+		var elements = [];
 		for (var i = 0 ; i < database.length ; i++)
 		{
-			var date = database[i]['birthdate'].split("-");
-			var row = document.createElement('tr');
-			var name = database[i]['firstname'] +" " + database[i]['lastname'];
-			var floor = database[i]['floor'];
-			var age = calculate_age(new Date(date[0],date[1],date[2]));
+			var element = getElements(database[i]);
+			elements.push(element);
 
-			var col1 = document.createElement('th');
-			col1.innerHTML = name;
+			var row = document.createElement('tr');
+
+			var id = document.createElement('td');
+			id.innerHTML = element.id;
+			// id.style.display = "block"
+			row.appendChild(id);
+			var col1 = document.createElement('td');
+			col1.innerHTML = element.name
 			row.appendChild(col1)
-			var col2 = document.createElement('th');
-			col2.innerHTML = floor;
+			var col2 = document.createElement('td');
+			col2.innerHTML = element.floor;
 			row.appendChild(col2)
-			var col3 = document.createElement('th');
-			col3.innerHTML = age;
+			var col3 = document.createElement('td');
+			col3.innerHTML = element.age;
 			row.appendChild(col3)
-			table.appendChild(row);
+			tbody.appendChild(row);
 		}
+		table.appendChild(tbody)
 	}
 
+	function getElements(db_element) {
+		var element = {}
+		element.id = db_element['residentID'];
+		element.name = db_element['firstname'] +" " + db_element['lastname'];
+		element.floor = db_element['floor'];
+		var date = db_element['birthdate'].split("-");
+		element.age  = calculate_age(new Date(date[0],date[1],date[2]));
+		return element;
+	}
 	function calculate_age(date) {
 		var diff_ms = Date.now() - date.getTime();
 		var age_dt = new Date(diff_ms);
 		return Math.abs(age_dt.getUTCFullYear() - 1970);
+	}
+
+	function init() {
+		$('#myTable tbody').on('click', 'tr', function() {
+			var id_td = this.firstChild;
+			var test = id_td.innerHTML;
+
+			console.log(window.location.href);
+			window.location.href =  "http://localhost/a18ux02/searchRes"+"?uid="+test;
+			<?php
+			if(isset($_GET['uid'])){
+				$id = $_GET['uid'];
+				redirect('resDash/'."?id=".$id);
+			}
+			?>
+		})
 	}
 </script>
 </body>
