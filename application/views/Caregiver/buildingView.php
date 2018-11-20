98708d2fbc361e394c61ddc212c19725f516c0aa
harpa.thrastardottir <harpa.thrastardottir@student.kuleuven.be>
2018-11-20 15:23:21 +0100

    Changed names of views to building->floor->room

--- application/views/Caregiver/buildingView.php
+++ application/views/Caregiver/buildingView.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: harpathrastardottir
- * Date: 18.11.2018
- * Time: 22:54
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database Searching</title>
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/floorView.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+
+<h1>Please select the desired floor</h1>
+
+<body>
+<ul id="myUL">
+    <!-- TODO: Add the correct links            -->
+    <!-- TODO: Get the floors from the database -->
+    <li><a href='roomView'>Floor 1</a></li>
+    <li><a href="roomView">Floor 2</a></li>
+    <li><a href="roomView">Floor 3</a></li>
+    <li><a href="roomView">Floor 4</a></li>
+    <li><a href="roomView">Floor 5</a></li>
+</ul>
+</body>
+</html>
--- application/views/Caregiver/floorView.php
+++ application/views/Caregiver/floorView.php
@@ -1,23 +1,131 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Database Searching</title>
-    <link rel="stylesheet" href="<?=base_url();?>assets/css/floorView.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-</head>
-<body>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-<h1>Please select the desired floor</h1>
+<div class="row" onclick="onClick()">
+    <div class="column">
+        <h2>Room 1</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 2</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 3</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 4</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 5</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 6</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 7</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 8</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 9</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 10</h2>
+        <p>Alice & Bob</p>
+    </div>
+</div>
 
-<body>
-<ul id="myUL">
-    <!-- TODO: Add the correct links            -->
-    <!-- TODO: Get the floors from the database -->
-    <li><a href='roomView'>Floor 1</a></li>
-    <li><a href="roomView">Floor 2</a></li>
-    <li><a href="roomView">Floor 3</a></li>
-    <li><a href="roomView">Floor 4</a></li>
-    <li><a href="roomView">Floor 5</a></li>
-</ul>
-</body>
-</html>
+<div class="row">
+    <div class="column" style="width : 100%; background : #bbb; height: 100px; cursor: auto";>
+
+    </div>
+</div>
+
+<div class="row" onclick="onClick()">
+    <div class="column">
+        <h2>Room 11</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 12</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 13</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 14</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 15</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 16</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 17</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 18</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 19</h2>
+        <p>Alice & Bob</p>
+    </div>
+    <div class="column">
+        <h2>Room 20</h2>
+        <p>Alice & Bob</p>
+    </div>
+</div>
+
+<style>
+    * {
+        box-sizing: border-box;
+        font-family: Roboto;
+    }
+
+    /* Create two equal columns that floats next to each other */
+    .column {
+        background: #003b46;
+        color: white;
+        float: left;
+        padding: 10px;
+        width : 10%;
+    }
+
+    .column:hover{
+        background: #009cad;
+        cursor:pointer;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+
+        content: "";
+        display: table;
+        clear: both;
+        width : 100%
+    }
+</style>
+
+<script>
+    function onClick()
+    {
+        window.location.href = 'singleRoomView';
+
+    }
+</script>
