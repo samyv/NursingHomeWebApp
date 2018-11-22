407005702c10f2ad194a3d8aab08108f46af68f4
Jef Meijvis <jef.meijvis@student.kuleuven.be>
2018-11-22 21:14:13 +0100

    Updated: buildingview now uses grid layout

--- application/views/Caregiver/buildingView.php
+++ application/views/Caregiver/buildingView.php
@@ -5,19 +5,32 @@
     <link rel="stylesheet" href="<?=base_url();?>assets/css/buildingView.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
-<body>
-
-<h1>Please select the desired floor</h1>
 
 <body>
-<ul id="myUL">
-    <!-- TODO: Add the correct links            -->
-    <!-- TODO: Get the floors from the database -->
-    <li><a href='floorView'>Floor 1</a></li>
-    <li><a href="floorView">Floor 2</a></li>
-    <li><a href="floorView">Floor 3</a></li>
-    <li><a href="floorView">Floor 4</a></li>
-    <li><a href="floorView">Floor 5</a></li>
-</ul>
+<div class = "grid-container">
+<!-- TODO: Add the correct links            -->
+<!-- TODO: Get the floors from the database -->
+
+    <div class = "bar">
+        <p>Navigation bar goes here</p>
+    </div>
+
+    <div class = "title">
+        <p>Please select the desired floor</p>
+    </div>
+
+    <div class = "list">
+        <a class = "listRow1" href='floorView'>Floor 1</a>
+        <a class = "listRow2" href='floorView'>Floor 2</a>
+        <a class = "listRow3" href='floorView'>Floor 3</a>
+        <a class = "listRow4" href='floorView'>Floor 4</a>
+        <a class = "listRow5" href='floorView'>Floor 5</a>
+    </div>
+
+    <div class = "footer">
+        <p>Footer goes here</p>
+    </div>
+
+</div>
 </body>
 </html>
