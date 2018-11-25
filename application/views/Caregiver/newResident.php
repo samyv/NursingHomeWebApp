8746dc276d3d5c9c1b49fa44f42229ed4910b13b
Tessa Luyten <tessa.luyten@student.kuleuven.be>
2018-11-25 15:56:06 +0100

    css add resident

--- application/views/Caregiver/newResident.php
+++ application/views/Caregiver/newResident.php
@@ -11,7 +11,6 @@
 <body>
 <form action="" method="post">
     <div class="grid-container">
-
         <div class = "resident">
             <h3>Resident information</h3>
         </div>
@@ -20,7 +19,7 @@
             <b>First name: </b>
         </div>
         <div class="firstname_input">
-            <input type="text" placeholder="Enter first name" name="firstname" required=""
+            <input type="text" placeholder="Enter first name" class = "form-control" name="firstname" required=""
                    value="<?php echo !empty($resident['firstname'])?$resident['firstname']:''; ?>">
             <?php echo form_error('firstname','<span class="help-block">','</span>'); ?>
         </div>
@@ -29,7 +28,7 @@
             <br>
         </div>
         <div class="lastname_input">
-            <input type="text" placeholder="Enter last name" name="lastname" required=""
+            <input type="text" placeholder="Enter last name" class = "form-control" name="lastname" required=""
                    value="<?php echo !empty($resident['lastname'])?$resident['lastname']:''; ?>">
             <?php echo form_error('lastname','<span class="help-block">','</span>'); ?>
         </div>
@@ -38,15 +37,15 @@
             <br>
         </div>
         <div class="birthday_input">
-            <input type="date" placeholder="" name="birthdate" required="">
+            <input type="date" placeholder="" name="birthdate" class = "form-control" required="">
             <?php echo form_error('birthdate','<span class="help-block">','</span>'); ?>
         </div>
         <div class="gender">
             <b>Gender: </b>
             <br>
         </div>
-        <div class="gender_input" name="gender" required="">
-            <select name="gender">
+        <div class="gender_input" name="gender"  required="">
+            <select name="gender" class = "form-control">
                 <option value="male">male</option>
                 <option value="female">female</option>
             </select>
@@ -57,7 +56,7 @@
             <br>
         </div>
         <div class="floor_input">
-            <input type="number" name="floor" placeholder="Enter floor number" required="">
+            <input type="number" name="floor"  class = "form-control" placeholder="Enter floor number" required="">
             <?php echo form_error('floor','<span class="help-block">','</span>'); ?>
         </div>
         <div class="room">
@@ -65,7 +64,7 @@
             <br>
         </div>
         <div class="room_input">
-            <input type="number" name="room" placeholder="Enter room number" required="">
+            <input type="number" name="room" class = "form-control" placeholder="Enter room number" required="">
             <?php echo form_error('room','<span class="help-block">','</span>'); ?>
         </div>
 
@@ -77,30 +76,37 @@
             <b>Name: </b>
         </div>
         <div class="contact_name_input">
-            <input type="text" name="full_name" placeholder="Enter contact person" required="">
+            <input type="text" name="full_name"  class = "form-control" placeholder="Enter contact person" required="">
             <br>
         </div>
         <div class="email">
             <b>Email: </b>
         </div>
         <div class="email_input">
-            <input type="text" name="email" placeholder="example@example.com">
+            <input type="text" name="email" class = "form-control" placeholder="example@example.com">
             <br>
         </div>
         <div class="phone">
             <b>Phone nr: </b>
         </div>
         <div class="phone_input">
-            <input type="tel" name="phone" placeholder="phonenumber">
+            <input type="tel" name="phone" class = "form-control" placeholder="phonenumber">
+        </div>
+
+        <div class="info">
+            <b>Modification date: </b>
+        </div>
+
+        <div class="info_input">
+            <input type="date" onload="getDate()" class="form-control" id="date"
+                   name="date">
         </div>
 
         <div class="buttons">
             <input type="submit" value="Add resident" name="saveSettings"/>
             <input type="button" value="Cancel" onclick="location.href='newResident'"/>
         </div>
-
-    </div>
-
+        </div>
 </form>
 </body>
 </html>
