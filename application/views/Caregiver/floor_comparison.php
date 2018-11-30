bd532c1eae12245c9bf5d277967484810102d5f9
Jef Meijvis <jef.meijvis@student.kuleuven.be>
2018-11-30 10:02:18 +0100

    Added a fancy line graph :D

--- application/views/Caregiver/floor_comparison.php
+++ application/views/Caregiver/floor_comparison.php
@@ -39,7 +39,6 @@
     </div>
 
     <div class = "graph">
-    <p> Here comes the visualisation</p>
     </div>
 
 </div>
@@ -47,8 +46,70 @@
 </html>
 
 <script>
-    console.log("loaded");
-    let g = d3.select("div");
-    g.append("p");
-    console.log(g);
+    /////////////////////////////////////////////////////////
+    //////              D3.JS GRAPH                     /////
+    /////////////////////////////////////////////////////////
+    var data = [];
+    let text_color = "white";
+    for (let i = 0 ; i < 100 ; i++) {
+        data.push(
+            {
+                date: new Date(i),
+                value: Math.sin(i/13.0)
+            });
+    }
+
+    var container = d3.select("body").selectAll("div.graph");
+
+    var svgSelection = container.append("svg")
+                                    .attr("width", 50)
+                                    .attr("height", 50);
+
+    var svgWidth = 600, svgHeight = 400;
+    var margin = { top: 20, right: 20, bottom: 30, left: 30 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    let svg = container.append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    var g = svg.append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")"
+        );
+
+    var x = d3.scaleTime().rangeRound([0, width]);
+    var y = d3.scaleLinear().rangeRound([height, 0]);
+
+    var line = d3.line()
+        .x(function(d) { return x(d.date)})
+        .y(function(d) { return y(d.value)})
+    x.domain(d3.extent(data, function(d) { return d.date }));
+    y.domain(d3.extent(data, function(d) { return d.value }));
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .select(".domain")
+        .remove();
+
+    g.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", text_color)
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Value");
+
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", text_color)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 </script>
