8bb8f237b0a4c95e7ab80a5978739a95619e3c6f
wouter.feremans <wouter.feremans@student.kuleuven.be>
2018-12-11 18:29:56 +0000

    Revert "Liyang Liu - question type"
    This reverts commit 6bd386f031499ad1e508993882010971eb6bfc18

--- application/views/Resident/questionPage.php
+++ application/views/Resident/questionPage.php
@@ -18,7 +18,7 @@
         </div>
     </div>
 
-    <div id="questionType">{sectionType} ({currentNum}/{totalNum})</div>
+    <div id="questionType">Question Type({currentNum}/{totalNum})</div>
 
     <div class="progress" id="progressbar">
         <div class="progress-bar progress-bar-striped active" role="progressbar"
@@ -183,13 +183,8 @@
         });
 
         $('#previous').click(async function(){
-            if(index > 1) {
-                index=previousQuestion;
-                window.location.href = '<?=base_url();?>resident/questionpage/'+index;
-            } else {
-                window.location.href = '<?=base_url();?>resident/tutorialpage'
-            }
-
+            if(index > 1) index=previousQuestion;
+            window.location.href = '<?=base_url();?>resident/questionpage/'+index;
         });
     });
 </script>
