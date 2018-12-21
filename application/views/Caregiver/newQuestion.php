158c1f7b82951a9a3ac5f560faaeb612545b1b24
samy.vanvooren <samy.vanvooren@student.kuleuven.be>
2018-12-21 11:34:18 +0100

    php almost done

--- application/views/Caregiver/newQuestion.php
+++ application/views/Caregiver/newQuestion.php
@@ -38,9 +38,11 @@
 		</div>
 	</div>
 <script type="text/javascript">
+	let lastSectionClicked = 1;
 	$(function () {
 		populateSections();
 		init();
+		lastSectionClicked = 1;
 
 		$('#addQuestion').on('click',function () {
 			var table = document.getElementById("qTable");
@@ -52,13 +54,71 @@
 // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
 			var cell1 = row.insertCell(0);
 			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
 
 // Add some text to the new cells:
 			cell1.innerHTML = amountOfQuestions+1;
 			cell2.innerHTML = "New Question";
-			cell2.setAttribute("colspan",2);
 			cell2.setAttribute("contenteditable",'true');
+			let saveQBut = $('<button/>',
+				{
+					text: 'save',
+					id: "saveQuestion",
+					click: function () {
+						if(this.innerHTML == "save") {
+							//FIND VALUES FOR NEW QUESTION
+							var lastQuestionTotal = questionsDB.slice(-1)[0];
+							console.log(lastQuestionTotal)
+							var lastID = parseInt(lastQuestionTotal.idQuestion);
+							//PK
+							let newQID = lastID + 1;
+							//FIND PosNum AND nextQuestion with lastQuestion of the same section
+							var lastQuestionSection = questionsDB.filter(e => e.questionType == lastSectionClicked).slice(-1)[0]
+							let newPosNum = parseInt(lastQuestionSection.positionNum) + 1;
+							let firstQOfNextSID = lastQuestionSection.nextQuestionId;
+							var newQuestion = {
+								idQuestion: newQID.toString(),
+								questionText: this.parentElement.parentElement.children[1].innerText,
+								nextQuestionId: firstQOfNextSID.toString(),
+								previousQuestionId: lastQuestionSection.idQuestion.toString(),
+								positionNum: newPosNum.toString(),
+								questionType: lastSectionClicked.toString()
+							}
+							lastQuestionSection.nextQuestionId = (lastID + 1).toString();
 
+							var firstQOfNextS = questionsDB.filter(e => e.idQuestion == firstQOfNextSID).slice(-1)[0]
+							firstQOfNextS.previousQuestionId = (lastID + 1).toString();
+							questionsDB.push(newQuestion)
+							this.innerHTML = "delete";
+						} else {
+							var questionToErase = questionsDB.filter(e => e.questionType == lastSectionClicked && e.positionNum == this.parentElement.parentElement.children[0].innerText).slice(-1)[0]
+							var nextQuestionID = questionToErase.nextQuestionId;
+							var prevQuestionID = questionToErase.previousQuestionId;
+							var prevQuestion = questionsDB.filter(e => e.idQuestion == prevQuestionID).slice(-1)[0]
+							var nextQuestion = questionsDB.filter(e => e.idQuestion == nextQuestionID).slice(-1)[0]
+							prevQuestion.nextQuestionId = nextQuestionID.toString();
+							nextQuestion.previousQuestionId = prevQuestionID.toString();
+							var key = null;
+							for (var prop in questionsDB)
+							{
+								if (questionsDB.hasOwnProperty(prop))
+								{
+									if (questionsDB[prop] === questionToErase)
+									{
+										key = prop;
+									}
+								}
+							}
+							delete questionsDB.key
+							clean(questionsDB)
+							console.log(questionsDB)
+							let tbody = this.parentElement.parentElement.parentElement;
+							let amountOfQuestions = tbody.children.length;
+							this.parentElement.parentElement.parentElement.parentElement.deleteRow(amountOfQuestions)
+						}
+					}
+				});
+			saveQBut.appendTo(cell3)
 		})
 		$('#addSection').on('click',function () {
 			var table = document.getElementById("sTable");
@@ -76,9 +136,6 @@
 			cell2.innerHTML = "New Section";
 			cell2.setAttribute("colspan",2);
 			cell2.setAttribute("contenteditable",'true');
-		})
-		$('#addSection').on('click',function () {
-			console.log("clickedS!")
 		})
 		$('#save').on('click',function () {
 			$.ajax({
@@ -98,8 +155,16 @@
 
 	var sections = "";
 	var questionsDB = "";
-	function populateSections()
-	{
+	function clean(obj) {
+		for (var propName in obj) {
+			if (obj[propName] === null || obj[propName] === undefined) {
+				delete obj[propName];
+			}
+		}
+		console.log("CLEAN DB")
+		console.log(obj)
+	}
+	function populateSections() {
 		sections = <?php echo json_encode($sections)?>;
 		var table = document.getElementById("sTable");
 		var tbody = document.createElement("tbody");
@@ -148,7 +213,6 @@
 		table2.appendChild(tbody)
 
 	}
-
 	function init() {
 		$('#sTable tbody').on('click', 'tr', function() {
 			if(this.firstChild.innerHTML != "Section") {
@@ -158,6 +222,7 @@
 	}
 
 	function setQuestionTable(id){
+		lastSectionClicked = id;
 		let questionsSections1 = questionsDB.filter(e => e.questionType == id);
 		var table2 = document.getElementById("qTable");
 		var old_tbody =table2.children[1];
