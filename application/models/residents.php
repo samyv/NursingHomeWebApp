964ff3f851b288f39b55a714fb2fdedd4e6af9c0
samy.vanvooren <samy.vanvooren@student.kuleuven.be>
2018-12-21 11:35:27 +0100

    Residents

--- application/models/residents.php
+++ application/models/residents.php
@@ -42,7 +42,7 @@ class Residents extends CI_Model
         $blob = fopen($data['filepath'], 'rb');
 
 
-//         insert contact person in DB
+        // insert contact person in DB
 
 
         $sql = "INSERT INTO a18ux02.Pictures (pictureMime, picture) VALUES ('$mime', :data)";
@@ -57,7 +57,7 @@ class Residents extends CI_Model
 
 
         //insert resident in resident table
-        $sql = "INSERT INTO a18ux02.Resident(residentID, firstname, lastname, birthdate, floor, room, gender,FK_ContactPerson,pictureId) VALUES (NULL, '$firstname','$lastname', '$birthdate', '$floor','$room','$gender','$idcp','$pictureid')";
+        $sql = "INSERT INTO a18ux02.Resident(residentID, firstname, lastname, birthdate, floor, room, gender,FK_ContactPerson, pictureId) VALUES (NULL, '$firstname','$lastname', '$birthdate', '$floor','$room','$gender','$idcp','$pictureid')";
         $insert = $this->db->query($sql);
         $id = $this->db->insert_id();
         $qrstring = $this->generateRandomString();
@@ -102,6 +102,7 @@ class Residents extends CI_Model
 		return 0;
     }
 
+    //test
 	function sendNotification(){
 		$text = $_SESSION['Resident']['residentID']." filled in a Questionnairy!";
 		$id = $_SESSION['Resident']['residentID'];
