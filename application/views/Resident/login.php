6869d33b2eaed90fc4893245d956b02b97b13dae
Jef Meijvis <jef.meijvis@student.kuleuven.be>
2019-01-24 15:44:52 +0100

    Fixed problems with base url on login page resident Removed redundant image assest for login page resident

--- application/views/Resident/login.php
+++ application/views/Resident/login.php
@@ -18,11 +18,10 @@
 </div>
 <video id="camera-stream" autoplay></video>
 <button onclick=autoLogin()></button>
-<img id = qrpicture class = qrpicture src= "../assets/images/qrcode2.png" alt="QR-code">
-<img id = qrmarker class = qrmarker src= "../assets/images/marker.png" alt="QR-code">
+<img id = qrpicture class = qrpicture src= '<?=base_url()?>assets/images/qrcode.png' alt="QR-code">
+<img id = qrmarker class = qrmarker src= '<?=base_url()?>assets/images/marker.png' alt="QR-code">
 <p class = explanation>Hou je QR-code in het groene vierkant</p>
 <script>
-
 
 	let scanner = new Instascan.Scanner({ video: document.getElementById('camera-stream') });
 	scanner.addListener('scan', function (content)
